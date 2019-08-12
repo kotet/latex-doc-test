fd11bdc1514e4b9c51187b2a7192ac388d3c05a1
Kotet <kotet0418@gmail.com>
2019-08-12 18:11:36 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,10 @@ mv ../doc.pdf ../.git .
 git config --global user.name "Circle CI"
 git config --global user.email "<>"
 git checkout gh-pages
+git pull
 git add -A
 git commit --allow-empty -m "[ci skip] Deploy by CI"
 
-mkdir ~/.ssh/ && echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
+mkdir -p ~/.ssh/ && echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 git push -f origin gh-pages
 cd -
